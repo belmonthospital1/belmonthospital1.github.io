64eeace6922f9e934000ae32fe511d1e46403b9a
Jhonfranco22 <83263190+Jhonfranco22@users.noreply.github.com>
2021-06-23 20:15:23 -0500

    Pagina Belmont
    Pagina Belmont

---
+++ Js/solicitud_cita.js
@@ -0,0 +1,105 @@
+function evaluar(){
+
+    var nom=document.getElementById("nombre").value;
+    var dni=document.getElementById("dni").value;
+    var mail=document.getElementById("correo").value;
+    var telef=document.getElementById("num").value;
+    var asunto=document.getElementById("asunto").value;
+    var comen=document.getElementById("comentarios").value;
+    
+    
+    var exnom=(/^[a-zA-Z\s]+$/);
+    var exdni=(/[0-9]{8}$/);
+    var exmail=/^(\w+)\@(\w+)\.([a-z]{2,4})$/i;
+    var extelef=/[0-9]{9}$/;
+    var exasunto=(/^[a-zA-Z\s]+$/);
+    var excomen=(/^[a-zA-Z\s]+$/);
+    
+    
+    /************************************************ */
+    if(!exnom.test(nom)){
+        document.getElementsByClassName("in1")[0].innerHTML="*No/valido";
+        document.getElementsByClassName("in1")[0].style.color="red";
+        
+    }
+     
+     else{
+        document.getElementsByClassName("in1")[0].innerHTML=" Valido";
+        document.getElementsByClassName("in1")[0].style.color="white";
+     }
+    
+     //***************************************** */
+    if(!exdni.test(dni)){
+        document.getElementsByClassName("in2")[0].innerHTML=" No/valido"
+        document.getElementsByClassName("in2")[0].style.color=" red";
+    }
+    
+    else{
+        document.getElementsByClassName("in2")[0].innerHTML=" Valido";
+        document.getElementsByClassName("in2")[0].style.color=" white";
+    }
+    
+    /********************************************* */
+    
+    if(!exmail.test(mail)){
+        document.getElementsByClassName("in3")[0].innerHTML=" No/valido"
+        document.getElementsByClassName("in3")[0].style.color=" red";
+    }else{
+    
+        document.getElementsByClassName("in3")[0].innerHTML=" Valido"
+        document.getElementsByClassName("in3")[0].style.color=" white";
+    }
+    
+    /********************************************* */
+    
+    if(!extelef.test(telef)){
+        document.getElementsByClassName("in4")[0].innerHTML=" No/valido"
+        document.getElementsByClassName("in4")[0].style.color=" red";
+        
+    }else{
+        document.getElementsByClassName("in4")[0].innerHTML=" Valido"
+        document.getElementsByClassName("in4")[0].style.color="white";
+    }
+    
+    /********************************************* */
+    
+    
+    if(!exasunto.test(asunto)){
+        document.getElementsByClassName("in5")[0].innerHTML="No/valido"
+        document.getElementsByClassName("in5")[0].style.color=" red";
+    }else{
+        document.getElementsByClassName("in5")[0].innerHTML="Valido"
+        document.getElementsByClassName("in5")[0].style.color="white";
+    }
+    
+    
+    
+    /********************************************* */
+    
+    
+    if(!excomen.test(comen)){
+        document.getElementsByClassName("in6")[0].innerHTML="No/valido"
+        document.getElementsByClassName("in6")[0].style.color=" red";
+    }else{
+        document.getElementsByClassName("in6")[0].innerHTML="Valido"
+        document.getElementsByClassName("in6")[0].style.color="white";
+    }
+    //******************************************************************************/ */
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+       
+    
+    
+    
+    
+    
+    
+    
+    
